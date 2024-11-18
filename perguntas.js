9d57758e72cb23aeec39a2e90afc5eb25c0d5452
Analice07 <161321118+Analice07@users.noreply.github.com>
2024-11-18 07:53:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Escola',
+    'O que é uma lousa?',
+    'é um recurso usado na sala de aula para anotar'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasi é Basilia'
+)
+
+criaCartao(
+    'Programação',
+    'o que é o botao f4?',
+    'abre a barra de endereços no Explorador de Arquivos do Windows'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'cite alguns pontos',
+    '!?.;'
+)
+
+criaCartao(
+    'saudacoes ',
+    'o que é saudacao?',
+    'palavras ou frase usada para cumprimentar alguem'
+)
+criaCartao(
+    'computador',
+    'O que é um computador?',
+    'maquina eletronica que processa dado e realiza tarefas atraves de intrucoes ou comandos'
+)
+
+criaCartao(
+    'vogais',
+    'quais sao as vogais?',
+    'a-e-i-o-u'
+)
+
+criaCartao(
+    'comidas',
+    'tipos de comidas',
+    'culinaria italiana, francesa, japonesa, portuguesa e mexicana'
+)
+ 
+criaCartao(
+    'marcas',
+    'cite algumas marcas',
+    'Itau, Brahma, Skol, Claro, Nubank, Bradesco e Vivo'
+)
